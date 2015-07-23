2122b3cf4cf6c7d7cfef9175484a04daaea78977
Don Armstrong <don@donarmstrong.com>
2015-07-23 15:44:31 -0700

    add make_srx_dir.R and start making all of the sample SRX directories for calling

--- tissue_expression/write_srx.R
+++ tissue_expression/write_srx.R
@@ -7,9 +7,9 @@ load(args[1])
 
 mk.output <-
     paste0("SRX_FILES=",paste(collapse=" ",chosen.samples[,SRX]),"\n")
-## for(srx in chosen.samples[,SRX]) {
-##     mk.output <- paste0(mk.output,
-##                         srx,"_SRR=",paste(collapse=" ",chosen.samples[SRX==srx,SRR]),"\n")
-## }
+for(srx in chosen.samples[,SRX]) {
+    mk.output <- paste0(mk.output,
+                        srx,"_SRR=",paste(collapse=" ",chosen.samples[SRX==srx,SRR]),"\n")
+}
 cat(mk.output,
     file=args[length(args)])
