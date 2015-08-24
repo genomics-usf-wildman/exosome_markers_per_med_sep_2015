c431f8a3b7ee5dd6c9e5697da418af7f49f798f8
Don Armstrong <don@donarmstrong.com>
2015-08-24 14:00:09 -0700

    include GTEx samples in tissue specific expression markers

--- tissue_specific_expression/R/categorize_samples.R
+++ tissue_specific_expression/R/categorize_samples.R
@@ -1,13 +1,22 @@
 library(data.table)
 
 args <- c("chosen_samples",
+          "GTEx_Data_V4_Annotations_SampleAttributesDS.txt",
           "categorized_samples")
 
 args <- commandArgs(trailingOnly=TRUE)
 
 load(args[1])
+gtex.samples <- fread(args[2])
 
-categorized.samples <- copy(chosen.samples)
+categorized.samples <- copy(chosen.samples[,list(Sample_Name,SRX)])
+
+gtex.samples <- gtex.samples[,list(SMTSD,SAMPID)]
+setnames(gtex.samples,c("SAMPID","SMTSD"),c("SRX","Sample_Name"))
+categorized.samples <-
+    rbind(categorized.samples,
+          gtex.samples)
+
 categorized.samples[,Sample_Group:=Sample_Name]
 categories <-
     list(
@@ -20,6 +29,17 @@ categories <-
         ignore.right.left=
             list(match=", (?:right|left)$",
                  replace=""),
+        ## remove (heart),
+        remove.heart.comma=
+            list(match="(?:heart), ",
+                 replace=""),
+        ## remove (cells|brain) -
+        remove.cells.dash=
+            list(match="(?:Cells|Brain|Breast|Cervix|Atery|Heart) - ",
+                 replace=""),
+        swap.dash=
+            list(match="^([A-Z]\\w+) - (.+)$",
+                 replace="\\1 \\2"),
         ## the one placenta from day 113 we'll just call placenta
         placenta.day.113=
             list(match="^placenta, day113 F$",
@@ -43,7 +63,8 @@ for (category in names(categories)) {
                                  categories[[category]]$replace,
                                  Sample_Group)]
 }
-                 
+
+categorized.samples[,Sample_Group:=tolower(Sample_Group)]
          
 save(categorized.samples,
      file=args[length(args)])
