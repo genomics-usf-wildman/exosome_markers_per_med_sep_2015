e2260f7620c850d7dfb7a30ae56c6b772884892b
Don Armstrong <don@donarmstrong.com>
2015-08-31 09:42:25 -0700

    set seeds, use repeats in the SVM call
    + Document how to eventually use ROC for multiple-classification problems

--- tissue_specific_expression/R/classification_svm.R
+++ tissue_specific_expression/R/classification_svm.R
@@ -29,6 +29,7 @@ gene.fpkm.wide$ut.pla.none <-
 ## set NA to zero
 gene.fpkm.wide[is.na(gene.fpkm.wide)] <- 0
 
+### seed generated with echo $RANDOM
 set.seed(10609)
 in_training <-
     createDataPartition(gene.fpkm.wide$ut.pla.none,
@@ -41,6 +42,8 @@ genes.training <-
 genes.testing <-
     gene.fpkm.wide[-in_training,]
 
+### seed generated with echo $RANDOM;
+set.seed(30224)
 knn.train <-
     train(form=ut.pla.none~.,
           data=genes.training[,-(1:2)],
@@ -49,12 +52,17 @@ knn.train <-
           tuneLength=5
           )
 
-
+### seed generated with echo $RANDOM;
+set.seed(29224)
+### multi-class ROC https://www.kaggle.com/c/forest-cover-type-prediction/forums/t/10573/r-caret-using-roc-instead-of-accuracy-in-model-training
 svm.train <-
     train(form=ut.pla.none~.,
           data=genes.training[,-(1:2)],
+          preProc=c("center","scale"),
           method="svmRadial",
-          trControl=trainControl(method="cv",number=10)
+          trControl=trainControl(method="cv",number=10,repeats=10,classProbs=TRUE),
+          tuneLength=8,
+          metric="ROC"
           )
 
 
