7ed877a35d8186a0618f68202b9356102f9ea4f4
Don Armstrong <don@donarmstrong.com>
2015-08-31 11:50:38 -0700

    add code to test the results of the svm and knn predictions

---
+++ tissue_specific_expression/R/svm_testing.R
@@ -0,0 +1,52 @@
+library(data.table)
+library(caret)
+library(reshape2)
+library(doMC)
+registerDoMC(cores=12)
+
+args <- c("svm_classification",
+          "svm_test_results"
+          )
+
+args <- commandArgs(trailingOnly=TRUE)
+
+load(args[1])
+
+
+### test the predictions using SVM
+gene.testing.predictions.svm.prob <-
+    predict(svm.train,
+            newdata=genes.testing,
+            type="prob")
+gene.testing.predictions.svm.prob$prediction <-
+    colnames(gene.testing.predictions.svm.prob)[apply(gene.testing.predictions.svm.prob,1,which.max)]
+
+gene.training.predictions.svm.prob <-
+    predict(svm.train,
+            newdata=genes.training,
+            type="prob")
+gene.training.predictions.svm.prob$prediction <-
+    colnames(gene.training.predictions.svm.prob)[apply(gene.training.predictions.svm.prob,1,which.max)]
+
+### test the predictions using the KNN
+gene.testing.predictions.knn.prob <-
+    predict(knn.train,
+            newdata=genes.testing,
+            type="prob")
+gene.testing.predictions.knn.prob$prediction <-
+    colnames(gene.testing.predictions.knn.prob)[apply(gene.testing.predictions.knn.prob,1,which.max)]
+
+gene.training.predictions.knn.prob <-
+    predict(knn.train,
+            newdata=genes.training,
+            type="prob")
+gene.training.predictions.knn.prob$prediction <-
+    colnames(gene.training.predictions.knn.prob)[apply(gene.training.predictions.knn.prob,1,which.max)]
+
+
+save(gene.testing.predictions.svm.prob,
+     gene.training.predictions.svm.prob,
+     gene.testing.predictions.knn.prob,
+     gene.training.predictions.knn.prob,
+     file=args[length(args)])
+
