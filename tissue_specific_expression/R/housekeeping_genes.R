b8dac950798fa38bbc14990d078e2b4c37b6962f
Don Armstrong <don@donarmstrong.com>
2016-01-14 11:13:56 -0800

    modify housekeeping genes to have a specific sd

--- tissue_specific_expression/R/housekeeping_genes.R
+++ tissue_specific_expression/R/housekeeping_genes.R
@@ -48,9 +48,15 @@ housekeeping.gene.levels <- function(gene) {
         gene=gene,
         tracking_id=gene.reads.wide[gene,tracking_id][1],
         mean=mean(gene.expression),
+        sd=sd(gene.expression),
         percentage=mean(gene.expression>50),
+        log.sd=sd(log(gene.expression[gene.expression>0])),
+        log.mean=mean(log(gene.expression[gene.expression>0])),
         percentage.10=mean(gene.expression>10),
-        percentage.1=mean(gene.expression>1)))
+        sd.10=sd(gene.expression[gene.expression>10]),
+        percentage.1=mean(gene.expression>1),
+        sd.1=sd(gene.expression[gene.expression>1])
+        ))
 }
 
 housekeeping.genes <-
@@ -61,6 +67,7 @@ housekeeping.genes <-
 housekeeping.genes <-
     rbindlist(housekeeping.genes)
 
+invisible(housekeeping.genes[,scaled.sd:=log.sd/log.mean])
 
 ### identify house keeping genes. These are genes which are expressed
 ### in pretty much every tissue. [Note that we'll have to be careful
@@ -69,11 +76,11 @@ housekeeping.genes <-
 ### We're using 60% here primarily to make sure that we get MT genes
 ### which in some cases have been excluded from alignments.
 
-### genes expressed with FPKM >= 50 in >= 60% of tissues 
+### genes expressed with FPKM >= 50 in >= 80% of tissues and with a scaled SD of less than 0.12
 
 housekeeping.genes <-
-    housekeeping.genes[percentage >= 0.6 |
-                           (grepl("^MT-",gene) & mean > 100)
+    housekeeping.genes[(percentage >= 0.8 & scaled.sd < 0.12) |
+                       (grepl("^MT-",gene) & mean > 100)
                       ,]
 setnames(housekeeping.genes,"gene","gene_short_name")
 
