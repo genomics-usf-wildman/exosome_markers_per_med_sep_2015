a12ee161b1c69d03519646e99e4107dd30703bde
Don Armstrong <don@donarmstrong.com>
2016-12-20 18:28:43 -0600

    include gtex counts

--- tissue_expression/R/combine_isoform_fpkms.R
+++ tissue_expression/R/combine_isoform_fpkms.R
@@ -14,6 +14,8 @@ args <- args[-length(args)]
 ### these are the isoform fpkm files
 isoform.files <- grep("_isoforms.fpkm_tracking",args,value=TRUE)
 
+gtex.file <- grep("GTEx_Analysis",args,value=TRUE)
+
 .get.srx <- function(x){
     gsub("(SRX\\d+)/.+","\\1",x)
 }
@@ -25,13 +27,29 @@ isoform.counts <- list()
 for (file in isoform.files) {
     srx.accession <- .get.srx(file)
     isoform.counts[[srx.accession]] <-
-        fread(file)
+        fread(file)[,list(tracking_id,gene_id,gene_short_name,FPKM,FPKM_status)]
     isoform.counts[[srx.accession]][,srx:=srx.accession]
     i <- i + 1
     setTxtProgressBar(pb,i)
 }
+gc()
 
-isoform.counts <- rbindlist(isoform.counts)
+gtex.counts <- fread(paste0("zcat ",gtex.file),skip="TargetID")
+
+gtex.counts[,TargetID:=gsub("\\.\\d+","",TargetID)]
+gtex.counts[,Gene_Symbol:=gsub("\\.\\d+","",Gene_Symbol)]
+gtex.counts[,Chr:=NULL]
+gtex.counts[,Coord:=NULL]
+setnames(gtex.counts,c("TargetID","Gene_Symbol"),c("tracking_id","gene_id"))
+gtex.counts <-
+    melt(gtex.counts,
+         id.vars=c("tracking_id","gene_id"),
+         value.name="FPKM",
+         variable.name="srx")
+gc()
+isoform.counts[["gtex"]] <- gtex.counts
+
+isoform.counts <- rbindlist(isoform.counts,fill=TRUE)
 
 save(file=results.file,
      isoform.counts)
