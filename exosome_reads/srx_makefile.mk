bb1c831454a2b14854c762e50a0c971e054b42bd
Don Armstrong <don@donarmstrong.com>
2017-01-04 12:35:35 -0800

    handle paired end reads properly

--- exosome_reads/srx_makefile.mk
+++ exosome_reads/srx_makefile.mk
@@ -12,23 +12,40 @@ REFERENCE_DIR:=../../tissue_expression/ref_seqs/
 NOTRIM=1
 include srx_info.mk
 
-
-SRR_FASTQ_FILES=$(patsubst %, %.fastq.gz,$(SRRS))
+ifeq ($(NREADS),1)
+SRR_FASTQ_FILES=$(foreach srr,$(SRRS),$(srr).fastq.gz)
+else
+SRR_FASTQ_FILES=$(foreach srr,$(SRRS),$(srr)_1.fastq.gz $(srr)_2.fastq.gz)
+endif
 
 make_srr_fastq: $(SRR_FASTQ_FILES)
 
+ifeq ($(NREADS),1)
 $(SRR_FASTQ_FILES): %.fastq.gz:
 	$(MODULE) load sratoolkit/2.3.5-2; \
-	fastq-dump -B --gzip $*;
-
+	fastq-dump --split-3 -B --gzip $*;
+else
+%_1.fastq.gz %_2.fastq.gz:
+	$(MODULE) load sratoolkit/2.3.5-2; \
+	fastq-dump --split-3 -B --gzip $*;
+endif
 
 SAMPLING=10
 READS=1000 5000 10000 50000
 FASTQ_FILES=$(foreach sample,$(shell seq 1 $(SAMPLING)),$(foreach read,$(READS),$(SRX)_r$(read)_s$(sample).fastq.gz))
 
+STAR_OPTIONS=--genomeLoad LoadAndKeep --sjdbGTFfile $(GTF) --quantMode GeneCounts
+
+ifeq ($(NREADS),1)
+READ_BIASER_OPTS:=
+else
+READ_BIASER_OPTS:=--paired
+endif
+
+
 make_fastq: ../read_biaser.pl $(SRR_FASTQ_FILES)
 	$< $(foreach read,$(READS),--read $(read)) --samplings $(SAMPLING) \
-		--output-prefix $(SRX) $(SRR_FASTQ_FILES)
+		--output-prefix $(SRX) $(READ_BIASER_OPTS) $(SRR_FASTQ_FILES)
 	gzip *_r*_s*.fastq;
 	touch $@
 
