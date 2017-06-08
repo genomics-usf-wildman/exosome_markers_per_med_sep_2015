ea9f4cfde225602ddf4f817fa15064da075fd018
Don Armstrong <don@donarmstrong.com>
2017-06-08 09:44:41 -0700

    use fwrite in array_to_text

--- common_r/array_to_text.R
+++ common_r/array_to_text.R
@@ -1,4 +1,5 @@
 ### $(R) $(ROPTS) -f array_to_text.R --args data_file_name array_name array_file.txt
+library("data.table")
 
 arguments <- commandArgs(trailingOnly=TRUE)
 data.file.name <- arguments[1]
@@ -6,7 +7,7 @@ data.array <- arguments[2]
 text.file.name <- arguments[3]
 
 load(data.file.name)
-eval(parse(text=c("write.table(",data.array,",",
+eval(parse(text=c("fwrite(",data.array,",",
              "file=text.file.name,",
              'sep="\\t")')
            )
