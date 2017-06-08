2ac31df180ef16ed94a03e187f095740664ff8b9
Don Armstrong <don@donarmstrong.com>
2017-06-08 11:00:26 -0700

    indicate housekeeping gene sources

--- tissue_specific_expression/R/housekeeping_genes_superset.R
+++ tissue_specific_expression/R/housekeeping_genes_superset.R
@@ -14,10 +14,15 @@ housekeeping.genes.eisenberg <-
 setnames(housekeeping.genes.eisenberg,
          c("hgnc_symbol","ensembl_gene_id"),
          c("gene_short_name","tracking_id"))
+housekeeping.genes.eisenberg[,`housekeeping_source`:="eisenberg 2013"]
+housekeeping.genes[,`housekeeping_source`:="tissue specificity"]
 housekeeping.genes.superset <-
-    rbindlist(list(housekeeping.genes,housekeeping.genes.eisenberg),
+    rbindlist(list(housekeeping.genes.eisenberg,
+                   housekeeping.genes
+                   ),
               use.names=TRUE,fill=TRUE)
 housekeeping.genes.superset <-
-    housekeeping.genes.superset[!duplicated(tracking_id),list(gene_short_name,tracking_id)]
+    housekeeping.genes.superset[!duplicated(tracking_id),
+                                list(gene_short_name,tracking_id,housekeeping_source)]
 
 save(housekeeping.genes.superset,file=args[length(args)])
